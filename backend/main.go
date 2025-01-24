24fa06c129f7a6691fa8d7f99b412796ce495713
Dylan J. Gerrits <gerritsdylan@gmail.com>
2025-01-24 15:19:45 +0100

    🎉 Setup: Add Makefile and update backend server to listen on port 4500

--- backend/main.go
+++ backend/main.go
@@ -1,10 +1,21 @@
 package main
 
 import (
-	hello "backend/packages"
+	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
-	who := "Go"
-	hello.Hello(who)
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		_, err := fmt.Fprintf(writer, "The API is up and running!")
+		if err != nil {
+			log.Fatal(fmt.Sprintf("An error occured while writing to the response: %v", err))
+		}
+	})
+
+	err := http.ListenAndServe(":4500", nil)
+	if err != nil {
+		log.Fatal(fmt.Sprintf("An error occured while starting the server: %v", err))
+	}
 }
--- frontend/src/StripePayment.jsx
+++ frontend/src/StripePayment.jsx
@@ -9,7 +9,7 @@ import "./StripePayment.css";
 // recreating the Stripe object on every render.
 // This is your test publishable API key.
 const stripePromise = loadStripe(
-  "pk_test_51N9TTzAx5un0U0mJbl9Ha8mi1TdVPRd4WgGSk5pGPfZEFqKoIlksgaznEOQ25cLCbRMfiU9osCcGQTCY1m4BKXp600xvQ3uQCz"
+  "pk_test_51QknNrG2UePXwLUWwp4ZD6r0yXzioRxXHvAXYT1DZPsUaS78OBYCvDR70jWv4XPLm5JDD2iPBxWB17RiGhwSznB3007xRO258q"
 );
 
 export default function StripePayment({ customerData }) {
